51a3588a8fdaef6ebbf454e7dac8c81518bb53c1
Jadjay Alolise <jerome.avond@alolise.org>
2014-10-29 00:55:22 +0100

    prettify coding : Comments !

--- session3/create_people.rb
+++ session3/create_people.rb
@@ -1,18 +1,22 @@
 #!/usr/bin/env ruby
 
+# Ask the user questions about person (Name, age, etc...)
+# Validate answer by checking validation rules
+# Return the validated answer or void
 def ask_question item, clas, validation
   valid = false
   while valid == false
     print "Please give %s (%s): " % [item, clas]
     response = gets.chomp
-    valid = true if validation[item.to_sym].match(response) or response.empty?
+    valid = true if validation[item].match(response) or response.empty?
   end
   return response
 end
 
-def create_person definition, validation
-  person = {}
-  resp = ""
+# Create the Hash person
+# Return false when one entry is void
+# Return the hash person otherwise
+def create_person definition, validation, person
   definition.each do |item,clas|
     resp = ask_question item, clas, validation
     return false if resp.empty?
@@ -21,27 +25,35 @@ def create_person definition, validation
   return person
 end
 
+# Create an array of persons called people
+# Return the array people
 def create_people definition, validation
   people = []
-  response = "temp"
   person = {}
-  # use bool
+
   while person
-    person = create_person definition, validation
+    person = create_person definition, validation, person
     people.push(person) if person != false
   end
   people
 end
 
+# Hash of definition
+# Key = type of information
+# Value = class of information
 definition = {
-               'name' => String,
-               'age' => Integer,
-               'sexe' => ['M','F'],
+               name: String,
+               age: Integer,
+               sexe: ['M','F'],
              }
+# Hash of validation
+# Key = type of information
+# Value = regex validating the information
 validation = {
                 name: /[a-zA-Z\-\s]+/,
                 age: /\d+/,
                 sexe: /M|F/,
              }
 
+# Main
 puts create_people definition, validation

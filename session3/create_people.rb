46a6995b76dca0837ab30a9127eef66fcc72b822
Jadjay Alolise <jerome.avond@alolise.org>
2014-10-29 00:33:33 +0100

    problem resolved : check response

--- session3/create_people.rb
+++ session3/create_people.rb
@@ -1,28 +1,33 @@
 #!/usr/bin/env ruby
 
-def ask_question item, clas
+def ask_question item, clas, validation
+  valid = false
+  while valid == false
     print "Please give %s (%s): " % [item, clas]
-    gets.chomp
+    response = gets.chomp
+    valid = true if validation[item.to_sym].match(response) or response.empty?
+  end
+  return response
 end
 
-def create_person definition
+def create_person definition, validation
   person = {}
   resp = ""
   definition.each do |item,clas|
-    resp = ask_question item,clas
+    resp = ask_question item, clas, validation
     return false if resp.empty?
     person.store(item, resp)
   end
   return person
 end
 
-def create_people definition
+def create_people definition, validation
   people = []
   response = "temp"
   person = {}
   # use bool
   while person
-    person = create_person definition
+    person = create_person definition, validation
     people.push(person) if person != false
   end
   people
@@ -33,5 +38,10 @@ definition = {
                'age' => Integer,
                'sexe' => ['M','F'],
              }
+validation = {
+                name: /[a-zA-Z\-\s]+/,
+                age: /\d+/,
+                sexe: /M|F/,
+             }
 
-puts create_people definition
+puts create_people definition, validation

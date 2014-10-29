ad4487526b2f1ab2dc037e283dad0e6f07d6575b
Jadjay Alolise <jerome.avond@alolise.org>
2014-10-29 02:22:19 +0100

    Improved with mongodb : Now able to memorize

---
+++ session3-bis/create_people_mongo.rb
@@ -0,0 +1,81 @@
+#!/usr/bin/env ruby
+
+require 'mongo'
+require 'pp'
+include Mongo
+
+host = 'localhost'
+port = MongoClient::DEFAULT_PORT
+
+client = MongoClient.new(host, port)
+db = client.db('create_people')
+coll = db.create_collection('people')
+
+# Ask the user questions about person (Name, age, etc...)
+# Validate answer by checking validation rules
+# Return the validated answer or void
+def ask_question item, clas, validation
+  valid = false
+  while valid == false
+    print "Please give %s (%s): " % [item, clas]
+    response = gets.chomp
+    valid = true if validation[item].match(response) or response.empty?
+  end
+  return response
+end
+
+# Create the Hash person
+# Return false when one entry is void
+# Return the hash person otherwise
+def create_person definition, validation
+  person = {}
+  definition.each do |item,clas|
+    resp = ask_question item, clas, validation
+    return false if resp.empty?
+    person.store(item, resp)
+  end
+  return person
+end
+
+# Create an array of persons called people
+# Return the array people
+def create_people definition, validation, coll
+  people = []
+  person = {}
+
+  while person
+    person = create_person definition, validation
+    coll.insert(person) if person != false
+    people.push(person) if person != false
+  end
+  people
+end
+
+# Hash of definition
+# Key = type of information
+# Value = class of information
+definition = {
+               name: String,
+               age: Integer,
+               sexe: ['M','F'],
+             }
+
+# Hash of validation
+# Key = type of information
+# Value = regex validating the information
+validation = {
+                name: /[a-zA-Z\-\s]+/,
+                age: /\d+/,
+                sexe: /M|F/,
+             }
+
+fields = {
+                _id: 0,
+                name: 1,
+                age: 1,
+                sexe: 1,
+             }
+# Main
+puts coll.find({},{fields: fields}).to_a
+#puts coll.find({"name" => "Rachida"},{ "_id": 0} ).to_a
+puts create_people definition, validation, coll
